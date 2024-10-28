f307f71df5744437de4b654e127c29a5a22927d4
Hh440 <harsh100045@gmail.com>
2024-10-28 17:05:53 +0530

    slite

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: '10mb' })); // Adjust the limit as needed
 
 // Endpoint to receive block data
 app.post('/blocks', async (req, res) => {
-  const blocksData = req.body.data; // Attempt to retrieve 'data' from the request body
+  const blockData = req.body.data; // Attempt to retrieve 'data' from the request body
 
   
   
